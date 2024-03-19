ba86a2f1d8cbc3844ed5df58805ddeb794232af3
Cipsis1 <152157968+Cipsis1@users.noreply.github.com>
2024-03-19 12:50:03 +0200

    Add files via upload

---
+++ pd.py
@@ -0,0 +1,28 @@
+import random
+from random import randint
+miklas = {'Četras kājas, viena cepure':'Galds', 'Zelta puķe krāsnī zied':'Uguns', 'Siers jūras dibenā':'Saule', 'Plikpauris gaisā':'Mēness', 'Galva no dzelzs, kājas no koka':'Āmurs'}
+
+atslegas = list(miklas.keys())
+skaits = 0
+pareizas_atbildes = 0
+
+print("Ja vēlies spēli beigt ievadi 'q'!")
+
+while True:
+    gadijums = randint(0, len(atslegas)-1)
+    minejums = input(f"{atslegas[gadijums]}: ")
+    if minejums.lower() == "q":
+        print("Paldies par spēli!")
+        break
+    elif pareizas_atbildes == 2:
+        print("Trīs pēc kārtas!")
+    elif skaits == 4:
+        break
+    elif minejums.capitalize() == miklas[atslegas[gadijums]]:
+        print("Pareizi!")
+        pareizas_atbildes += 1
+    else:
+        print("Nepareizi!")
+    skaits +=1
+
+print(f"Tu pareizi atbildēji uz {pareizas_atbildes} no {skaits + 1} jautājumiem!")
