482292a2c853977075264bcb8c55d9afe64251c5
Cipsis1 <152157968+Cipsis1@users.noreply.github.com>
2024-03-22 21:51:33 +0200

    Add files via upload
    Pievienots random.choise
    Problēmas ar minejums =

--- pd.py
+++ pd.py
@@ -1,16 +1,23 @@
 import random
-from random import randint
+# from random import randint
 miklas = {'Četras kājas, viena cepure':'Galds', 'Zelta puķe krāsnī zied':'Uguns', 'Siers jūras dibenā':'Saule', 'Plikpauris gaisā':'Mēness', 'Galva no dzelzs, kājas no koka':'Āmurs'}
 
 atslegas = list(miklas.keys())
 skaits = 0
 pareizas_atbildes = 0
 
+def randomMikla():
+    return
+random.choice(list(miklas.keys()))
+
 print("Ja vēlies spēli beigt ievadi 'q'!")
 
 while True:
-    gadijums = randint(0, len(atslegas)-1)
-    minejums = input(f"{atslegas[gadijums]}: ")
+    # gadijums = randint(0, len(atslegas)-1)
+    # minejums = input(f"{atslegas[gadijums]}: ")
+    gadijums = randomMikla()
+    print(gadijums)
+    minejums = input(f"{miklas[gadijums]}: ")
     if minejums.lower() == "q":
         print("Paldies par spēli!")
         break
