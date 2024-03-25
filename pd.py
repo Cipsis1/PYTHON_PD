669fcfbdd3a99cf2a9829d5109ebaa13d6d99062
Cipsis1 <152157968+Cipsis1@users.noreply.github.com>
2024-03-25 10:19:44 +0200

    Add files via upload

--- pd.py
+++ pd.py
@@ -1,9 +1,13 @@
 import random
-miklas = {'Četras kājas, viena cepure':'Galds', 'Zelta puķe krāsnī zied':'Uguns', 'Siers jūras dibenā':'Saule', 'Plikpauris gaisā':'Mēness', 'Galva no dzelzs, kājas no koka':'Āmurs'}
+miklas = {'Četras kājas, viena cepure':'Galds', 'Zelta puķe krāsnī zied':'Uguns', 'Siers jūras dibenā':'Saule', 'Plikpauris gaisā':'Mēness', 'Galva no dzelzs, kājas no koka':'Āmurs', 'Zoss ar četriem deguniem':'Spilvens', 'Mežā un mājā vienā vārdā':'Ieva', 'Nekad nesadeg, nekad nenoslīkst':'Ēna', 'Asiņu tēvs līkām kajām':'Ods', 'Otram rāda pats neredz':'Brilles'}
 
 atslegas = list(miklas.keys())
 skaits = 0
 pareizas_atbildes = 0
+
+def punktu_skaitisana(punkti):
+    global pareizas_atbildes
+    pareizas_atbildes += punkti
 
 def randomMikla():
     return random.choice(atslegas)
@@ -13,16 +17,17 @@ print("Ja vēlies spēli beigt ievadi 'q'!")
 while True:
     gadijums = randomMikla()
     minejums = input(f"{gadijums}: ")
-    if minejums.lower() == "q":
+    if skaits == 4:
+        break
+    elif minejums.lower() == "q":
         print("Paldies par spēli!")
         break
-    elif pareizas_atbildes == 2:
+    elif pareizas_atbildes == 2 and pareizas_atbildes < 3:
         print("Trīs pēc kārtas!")
-    elif skaits == 4:
-        break
+        punktu_skaitisana(1)
     elif minejums.capitalize() == miklas[gadijums]:
         print("Pareizi!")
-        pareizas_atbildes += 1
+        punktu_skaitisana(1)
     else:
         print("Nepareizi!")
     skaits +=1
